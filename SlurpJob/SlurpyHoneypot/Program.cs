5eb63d1190f0349c998d11f50ba744a8a0895c9c
Richard Molnar <molnarriso@gmail.com>
2023-04-22 16:30:04 +0200

    Initial project commit

---
+++ SlurpJob/SlurpyHoneypot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SlurpyHoneypot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
